8a8e64939c16280587040d94d45a9a18036c0900
tony <antonio.villegas.0101@gmail.com>
2020-08-28 20:56:22 -0500

    early form to create new locker

--- src/components/createLocker/createLocker.js
+++ src/components/createLocker/createLocker.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Form1 from './createLockerForm1';
+import Slide from '@material-ui/core/Slide';
 
 const useStyles = theme => ({
     root: {
         width: '80%',
-        maxWidth: '30em',
-        margin: '0 auto',
+        maxWidth: '40em',
+        margin: '5em auto',
         textAlign: 'center'
     },
     title: {
         color: 'var(--color-theme-1)',
         fontWeight: '600',
     },
-    
+
 });
 
 class createLocker extends Component {
+    
+    handleEvent() {
+        console.log(this.props);
+    }
     render() {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
                 <h2 className={classes.title}>Create new locker</h2>
-                <Form1/>
+                <Slide direction="left" in={true} mountOnEnter unmountOnExit>
+                    <Form1 />
+                </Slide>
 
             </div>
         );
--- src/components/createLocker/createLockerForm1.js
+++ src/components/createLocker/createLockerForm1.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import EyeOff from '../../assets/img/eye-closed.svg';
+import Eye from '../../assets/img/eye-open.svg';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import OutlinedInput from '@material-ui/core/FilledInput';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = theme => ({
     textField: {
@@ -8,27 +16,46 @@ const useStyles = theme => ({
         margin: '1em auto 0 auto',
     },
     categoryContainer: {
-        marginTop:'2em',
-        display:'flex',
-        flexFlow:'wrap',
+        marginTop: '2em',
+        display: 'flex',
+        flexFlow: 'wrap',
         backgroundColor: 'var(--color-theme-3)',
         width: '100%',
         minHeight: '1em',
-        paddingBottom:'2em',
+        paddingBottom: '2em',
     },
-    textFieldMedium:{
+    textFieldMedium: {
         width: '90%',
         margin: '2em auto 0 auto',
     },
-    textFieldSmall:{
+    textFieldSmall: {
         width: '40%',
         margin: '2em auto 0 auto',
     },
+    iconButton:{
+        width:'1em',
+        marginRight:'0',
+        padding:'0',
+    }
 
 });
 
 class createLocker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPassword: false,
+        }
+        this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
+    }
+    handleClickShowPassword() {
+        let showPassword = this.state.showPassword;
+        this.setState({
+            showPassword: !showPassword
+        })
+    }
     render() {
+        const {showPassword} = this.state;
         const { classes } = this.props;
         return (
             <form className={classes.root} noValidate autoComplete="off">
@@ -36,7 +63,36 @@ class createLocker extends Component {
                 <div className={classes.categoryContainer}>
                     <TextField className={classes.textFieldMedium} id="filled-basic" label="Category" variant="filled" />
                     <TextField className={classes.textFieldSmall} id="filled-basic" label="Title" variant="filled" />
-                    <TextField className={classes.textFieldSmall} id="filled-basic" label="Secret" variant="filled" />
+                    <FormControl variant="filled" className={classes.textFieldSmall}>
+                            <InputLabel htmlFor="filled-adornment-password">Secret</InputLabel>
+                            <OutlinedInput
+                                type={showPassword ? 'text' : 'password'}
+                                onChange={this.handleChangeField}
+                                helpertext=""
+                                
+                                error=""
+                                id="password"
+                                variant="filled"
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        className={classes.iconButton}
+                                            aria-label="toggle password visibility"
+                                            onClick={this.handleClickShowPassword}
+                                        >
+                                            {showPassword ?
+                                                <img src={Eye} alt="ver contraseña" />
+                                                :
+                                                <img src={EyeOff} alt="ocultar contraseña" />
+                                            }
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                labelWidth={86} />
+                            <Typography variant="caption" >
+                                error
+                            </Typography>
+                        </FormControl>
 
                 </div>
             </form>
